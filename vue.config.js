e7201ae7e9dc5230a80bfb88cb31ecc9de158388
hiraith <hiraith@naver.com>
2020-02-12 20:22:52 +0900

    indexPath: "./index.html",

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   assetsDir: "static",
-  outputDir: "../docs/static",
-  indexPath: "../index.html",
+  outputDir: "./static",
+  indexPath: "./index.html",
 //  devServer: {
 //    proxy: "http://localhost:4200",
 //    overlay: false
